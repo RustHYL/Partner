1f27c65c0aa6f6386eb0edc7808f5dded97393f4
RustHYL <95454209+RustHYL@users.noreply.github.com>
2023-10-06 07:33:16 +0800

    frontend-design
    frontend-design

--- partner-matching-frontend/src/layouts/BasicLayout.vue
+++ partner-matching-frontend/src/layouts/BasicLayout.vue
@@ -4,7 +4,6 @@
       left-arrow
       @click-left="onClickLeft"
       @click-right="onClickRight"
-      fixed
   >
     <template #right>
       <van-icon name="search" size="18" />
@@ -12,30 +11,24 @@
   </van-nav-bar>
 
   <div id="content">
-    <template v-if="active === 'index'">
-      <Index />
-    </template>
-    <template v-if="active === 'team'">
-      <Team />
-    </template>
+    <router-view />
   </div>
-
-  <van-tabbar v-model="active" @change="onChange" fixed>
-    <van-tabbar-item icon="home-o" name="index">主页</van-tabbar-item>
-    <van-tabbar-item icon="search" name="team">队伍页</van-tabbar-item>
-    <van-tabbar-item icon="friends-o" name="user">个人</van-tabbar-item>
+  <van-tabbar route @change="onChange">
+    <van-tabbar-item to="/" icon="home-o" name="index">主页</van-tabbar-item>
+    <van-tabbar-item to="/team" icon="search" name="team">队伍页</van-tabbar-item>
+    <van-tabbar-item to="/user" icon="friends-o" name="user">个人</van-tabbar-item>
   </van-tabbar>
 </template>
 
 <script setup lang="ts">
 import {showToast} from "vant";
-import {ref} from "vue";
-import Index from "../pages/Index.vue"
-import Team from "../pages/Team.vue";
+import {useRouter} from "vue-router";
 
+const router = useRouter();
 const onClickLeft = () => alert("左")
-const onClickRight = () => alert("右");
-const active = ref("index");
+const onClickRight = () => {
+  router.push('/search')
+};
 const onChange = (index:any) => showToast(`标签 ${index}`);
 </script>
 
---
+++ partner-matching-frontend/src/pages/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <form action="/">
+    <van-search
+        v-model="searchText"
+        show-action
+        placeholder="请输入要搜索的标签"
+        @search="onSearch"
+        @cancel="onCancel"
+
+    />
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { showToast } from 'vant';
+
+const searchText = ref('');
+const onSearch = (val) => showToast(val);
+const onCancel = () => showToast('取消');
+</script>
+
+<style scoped>
+
+</style>
