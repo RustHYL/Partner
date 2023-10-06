eec9227cf1afac65fd773c8ec73ac8fe5989fb6d
RustHYL <95454209+RustHYL@users.noreply.github.com>
2023-10-07 01:23:14 +0800

    fontend
    editPage v0.0.1

--- partner-matching-frontend/src/pages/UserEditPage.vue
+++ partner-matching-frontend/src/pages/UserEditPage.vue
@@ -1,11 +1,36 @@
 <template>
+  <van-form @submit="onSubmit">
+    <van-cell-group inset>
+      <van-field
+          v-model="editUser.currentValue"
+          :name="editUser.editKey"
+          :label="editUser.editName"
+          :placeholder="`请输入${editUser.editName}`"
+      />
+    </van-cell-group>
+    <div style="margin: 16px;">
+      <van-button round block type="primary" native-type="submit">
+        提交
+      </van-button>
+    </div>
+  </van-form>
 
 </template>
 
 <script setup lang="ts">
-import {useRouter} from "vue-router";
+import {useRoute} from "vue-router";
+import {ref} from "vue";
 
-const router = useRouter();
+const route = useRoute();
+const editUser = ref({
+  editKey: route.query.editKey,
+  editName: route.query.editName,
+  currentValue: route.query.currentValue
+})
+const onSubmit = (values) => {
+  //todo 把editKey,currentValue,editName提交到后台
+  console.log('onSubmit',values);
+};
 
 </script>
 
--- partner-matching-frontend/src/pages/UserPage.vue
+++ partner-matching-frontend/src/pages/UserPage.vue
@@ -1,13 +1,13 @@
 <template>
   <div style="text-align: start">
-    <van-cell title="昵称" is-link :value="user.username" @click="toEdit('username',user.username)" />
+    <van-cell title="昵称" is-link :value="user.username" @click="toEdit('username','昵称',user.username)" />
     <van-cell title="用户账号" :value="user.userAccount" />
     <van-cell title="头像" is-link to="user/edit">
       <img style="height: 48px; width: 48px" :src="user.avatarUrl" />
     </van-cell>
-    <van-cell title="性别" is-link to="user/edit" :value="user.gender" />
-    <van-cell title="电话" is-link to="user/edit" :value="user.phone" />
-    <van-cell title="邮箱" is-link to="user/edit" :value="user.email" />
+    <van-cell title="性别" is-link :value="user.gender" @click="toEdit('gender','性别',user.gender)" />
+    <van-cell title="电话" is-link :value="user.phone" @click="toEdit('phone','电话',user.phone)" />
+    <van-cell title="邮箱" is-link :value="user.email" @click="toEdit('email','邮箱',user.email)" />
     <van-cell title="校验编号" :value="user.verifyCode" />
     <van-cell title="注册时间" :value="user.createTime.toISOString()" />
   </div>
@@ -30,11 +30,12 @@ const user = {
 }
 
 const router = useRouter();
-const toEdit = (editKey: string, currentValue: string) => {
+const toEdit = (editKey: string,editName: string, currentValue: string) => {
   router.push({
     path: '/user/edit',
     query: {
       editKey,
+      editName,
       currentValue,
     }
   })
